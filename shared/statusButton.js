791f7ce5e9a7b6ff67a358b4905f7492abae3266
ZC <zc@ZCs-MacBook-Air.local>
2020-10-16 05:02:10 -0400

    Added colors to buttons, added submit button

--- shared/statusButton.js
+++ shared/statusButton.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { CommonActions } from '@react-navigation/native';
 import { globalStyles } from '../styles/global';
 
 export default function StatusButton(props) {
@@ -10,17 +11,39 @@ export default function StatusButton(props) {
         setSelected(!isSelected);
     };
 
-    return (
-        <TouchableOpacity
-            onPress={pressHandler}
-            style={[
-                globalStyles.statusButton,
-                isSelected && globalStyles.statusButtonSelected
-            ]}
-        >
-            <View style={globalStyles.cardContent}>
-                { props.children }
-            </View>
-        </TouchableOpacity>
-    );
+    const submitHandler = () => {
+        alert('Report is submitted!');
+    };
+
+    if(props.name == 'SUBMIT'){
+        return (
+            <TouchableOpacity
+                onPress={submitHandler}
+                style={[
+                    props.buttonColor,
+                    globalStyles.statusButton,
+                ]}
+            >
+                <View style={globalStyles.cardContent}>
+                    { props.children }
+                </View>
+            </TouchableOpacity>
+        );
+    } 
+    else {
+        return (
+            <TouchableOpacity
+                onPress={pressHandler}
+                style={[
+                    props.buttonColor,
+                    globalStyles.statusButton,
+                    isSelected && globalStyles.statusButtonSelected
+                ]}
+            >
+                <View style={globalStyles.cardContent}>
+                    { props.children }
+                </View>
+            </TouchableOpacity>
+        );
+    }
 }
