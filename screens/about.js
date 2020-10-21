bbc240c5a492aee27e08c71ae25c8a6d69523016
sinai228 <purplelove228@gmail.com>
2020-10-21 11:22:35 +0900

    created AboutStack

--- screens/about.js
+++ screens/about.js
@@ -10,7 +10,7 @@
 
 // import functions and libraries
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 
 // import custom functions and styles
 import { globalStyles } from '../styles/global';
@@ -22,7 +22,7 @@ export default function About({ navigation }) {
       <Text style={globalStyles.locationTitle}>FreeSpace{'\n'}</Text>
       <Text> FreeSpace, our user-driven mobile app, serves to accomplish exactly that. {'\n'} It is tailored specifically towards Calvin University students in search of an available area on campus.</Text>
       <Text style={globalStyles.locationTitle} >Developers: {'\n'}</Text>
-      <Text>Esther Cha {'\n'}   Zachary Chin{'\n'}    Seongil (Leo) Kim{'\n'}   YoungIn Kim{'\n'}   Sinai Park{'\n'}    Benjamin Westerhof</Text>
+      <Text>    Esther Cha {'\n'}   Zachary Chin{'\n'}    Seongil (Leo) Kim{'\n'}   YoungIn Kim{'\n'}   Sinai Park{'\n'}    Benjamin Westerhof</Text>
       {/* <Button
         onPress={() => alert('Report is submitted!')}
         title="SUBMIT"
@@ -32,3 +32,16 @@ export default function About({ navigation }) {
 
   )
 }
+
+
+const styles = StyleSheet.create({
+  appTitle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  developers: {}
+})
